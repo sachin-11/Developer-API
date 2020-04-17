79918c01aa9ae718a8f500f7f3a697c009a10746
sachin singh <sachin.singh@antino.io>
2020-04-17 11:51:05 +0530

    init commit

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -41,4 +41,39 @@ router.post(
   }
 );
 
+// @route GET api/posts
+// @desc get all Posts
+// @access Private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Posts.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+// @route GET api/posts
+// @desc get all Posts
+// @access Private
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(400).json({ msg: "Post is not defind" });
+    }
+
+    res.json(post);
+  } catch (error) {
+    consol.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
+
 module.exports = router;
